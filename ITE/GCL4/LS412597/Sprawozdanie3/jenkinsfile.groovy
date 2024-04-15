f981441e4799c2426e7a219921fb99d749bbacc0
Łukasz Sawina <lukisaw01@gmail.com>
2024-04-16 00:30:36 +0200

    LS412597 jenkinsfile

---
+++ ITE/GCL4/LS412597/Sprawozdanie3/jenkinsfile.groovy
@@ -0,0 +1,23 @@
+pipeline {
+    agent any
+    
+    stages {
+        stage('Build') {
+            steps {
+                script {
+                    // Budowa aplikacji z użyciem pliku Dockerfile nodeapp_builder.Dockerfile
+                    docker.build('nodeapp_build', '-f ../Sprawozdanie2/nodeapp/nodeapp_builder.Dockerfile .')
+                }
+            }
+        }
+        
+        stage('Test') {
+            steps {
+                script {
+                    // Testowanie aplikacji z użyciem pliku Dockerfile nodeapp_tester.Dockerfile
+                    docker.build('nodeapp_test', '-f ../Sprawozdanie2/nodeapp/nodeapp_tester.Dockerfile .')
+                }
+            }
+        }
+    }
+}
