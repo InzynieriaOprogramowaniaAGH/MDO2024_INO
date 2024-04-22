6d35395dd1af6570e171d133a36f8281136658a7
Łukasz Sawina <lukisaw01@gmail.com>
2024-04-22 17:44:37 +0200

    LS412597 Dockerfiles for spr3

--- ITE/GCL4/LS412597/Sprawozdanie3/Jenkinsfile.groovy
+++ ITE/GCL4/LS412597/Sprawozdanie3/Jenkinsfile.groovy
@@ -5,11 +5,8 @@ pipeline {
         stage('Build') {
             steps {
                 script {
-                    sh 'ls -la'
-                }
-                script {
                     // Budowa aplikacji z użyciem pliku Dockerfile nodeapp_builder.Dockerfile
-                    docker.build('nodeapp_build', '-f ITE/GCL4/LS412597/Sprawozdanie2/nodeapp/nodeapp_builder.Dockerfile .')
+                    docker.build('takenode_build', '-f ITE/GCL4/LS412597/Sprawozdanie3/builder.Dockerfile .')
                 }
             }
         }
@@ -18,7 +15,7 @@ pipeline {
             steps {
                 script {
                     // Testowanie aplikacji z użyciem pliku Dockerfile nodeapp_tester.Dockerfile
-                    docker.build('nodeapp_test', '-f ITE/GCL4/LS412597/Sprawozdanie2/nodeapp/nodeapp_tester.Dockerfile .')
+                    docker.build('takenode_test', '-f ITE/GCL4/LS412597/Sprawozdanie3/tester.Dockerfile .')
                 }
             }
         }
