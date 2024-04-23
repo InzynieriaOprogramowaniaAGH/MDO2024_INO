5b620323dff6b5883f751c0b52ad9a4a24bce05f
Łukasz Sawina <lukisaw01@gmail.com>
2024-04-23 18:26:52 +0200

    LS412597 sprawko3

--- ITE/GCL4/LS412597/Sprawozdanie3/Jenkinsfile.groovy
+++ ITE/GCL4/LS412597/Sprawozdanie3/Jenkinsfile.groovy
@@ -29,13 +29,17 @@ pipeline {
                     def appImage = docker.build('nodeapp_deploy', '-f ITE/GCL4/LS412597/Sprawozdanie3/deploy.Dockerfile .')
 
                     // Uruchomienie kontenera w tle
-                    def container = appImage.run("--rm -d -p 3000:3000 --network=deploy --name app")
+                    def container = appImage.run("-d -p 3000:3000 --network=deploy --name app")
 
                     // Dajemy chwilę czasu na uruchomienie kontenera
                     sh "sleep 10" // Czekaj 10 sekund
 
                     // Sprawdzenie, czy aplikacja działa, wykonując żądanie HTTP
                     sh 'docker run --rm --network=deploy curlimages/curl:latest -L -v  http://app:3000'
+
+                    sh 'docker stop app'
+
+                    sh 'docker container rm app'
 
                     sh 'docker network rm deploy'
                 }
