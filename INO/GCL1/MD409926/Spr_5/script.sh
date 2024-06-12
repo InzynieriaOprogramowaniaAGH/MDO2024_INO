6f35ce1af1cab8a0acf55fdbb1244b088517e41b
dmaciej409926 <dmaciej@student.agh.edu.pl>
2024-06-12 17:05:40 +0000

    MD409926 - update sprawozdania5

---
+++ INO/GCL1/MD409926/Spr_5/script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DEPLOYMENT_NAME="deployment"
+TIMEOUT=60
+
+minikube kubectl -- apply -f ./deployment.yaml
+
+minikube kubectl get deployments 
+
+if minikube kubectl -- rollout status deployment/$DEPLOYMENT_NAME --timeout=${TIMEOUT}s > /dev/null 2>&1; then
+  echo "Wdrożenie wykonane poniżej minuty."
+  exit 0
+else
+  echo "Wdrożenie trwało dłużej niż minuta."
+  exit 1
+fi
